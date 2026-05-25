a99dcd5c551467d9a0293ae94c04254b9091ea81
Balamanikandan <bala34510@gmail.com>
2026-05-25 18:56:12 +0530

    removed trailing stash in base_url

--- src/components/constants.js
+++ src/components/constants.js
@@ -2,4 +2,4 @@
 
 // For now changing the BASE_URL TO /api, once learn about env we nnned to  modify this BASE_URL
 
-export const BASE_URL = "/api/"; // /api/ means it  take the frontend domain name eg 123.43.45/api/ as base_url
+export const BASE_URL = "/api"; // /api means it  take the frontend domain name eg 123.43.45/api/ as base_url
